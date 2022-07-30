0371d57041eba50df9ae3f959c8cbb25590b08f4
Joao Pedro <op.carvalho11@gmail.com>
2022-07-30 15:50:11 -0300

    Adding yarn and some middlewares

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
-const checkCPF = require('./tools/checkCPF');
-const locateUser = require('./tools/locateUser');
 const express = require('express');
+const cors = require('cors');
+const checkCPF = require('./tools/checkCPF');
 const { v4: uuidv4 } = require('uuid');
 const app = express();
 
@@ -9,27 +9,40 @@ const customers = [];
 const today = new Date();
 const time = `${today.getUTCDate()}-${today.getUTCMonth()}-${today.getFullYear()};`;
 
+app.use(cors());
 app.use(express.json());
 
-app.post('/account/client/create', (req, res) => {
+function customerAlreadyExist(req, res, next) {
   if (!req.body) {
     return res.send('Por favor, insira um cpf e um nome para cadastro!');
   } else if (!req.body.cpf || !req.body.name) {
     return res.send('Por favor, insira o cpf e o nome corretamente!');
   }
-
-  const { cpf, name } = req.body;
-
-  const customerAlreadyExist = customers.some(
-    (customer) => customer.cpf === cpf
-  );
-  if (customerAlreadyExist) {
-    return res.status(400).send('CPF já cadastrado no sistema');
-  }
-
-  if (checkCPF(cpf) == false) {
+  if (checkCPF(req.body.cpf) === false) {
     return res.status(400).send('Por favor, insira um cpf válido');
   }
+  if (
+    (customerAlreadyExist = customers.some(
+      (customer) => customer.cpf === req.body.cpf
+    ))
+  ) {
+    return res
+      .status(400)
+      .send(`CPF "${req.body.cpf}" já é cadastrado no sistema.`);
+  }
+  next();
+}
+
+function checkExistClient(req, res, next) {
+  const client = customers.find((customer) => [customer.id === req.params.id]);
+  if (!client) {
+    return res.send(`Usuário "${req.params.id}" não encontrado`);
+  }
+  next();
+}
+
+app.post('/account/client/create', customerAlreadyExist, (req, res) => {
+  const { cpf, name } = req.body;
 
   const extrato = [{ time, transacao: 0 }];
 
@@ -38,13 +51,8 @@ app.post('/account/client/create', (req, res) => {
   return res.status(201).send('Usuário criado com sucesso!');
 });
 
-app.post('/account/client/transaction/:id', (req, res) => {
-  const client = locateUser(customers, req.params.id);
-  console.log(client);
+app.post('/account/client/transaction/:id', checkExistClient, (req, res) => {
   let newExtract = {};
-  if (client == null) {
-    res.send('Cliente não encontrado');
-  }
   if (req.body.valor == 0 || req.body.move == '') {
     res.send(
       'Por favor informe o valor a ser movimentado e se é saque ou deposito!!'
@@ -74,15 +82,13 @@ app.get('/account/client/', (req, res) => {
   return res.send(customers);
 });
 
-app.get('/account/client/:id', (req, res) => {
-  if (locateUser(customers, req.params.id)) {
-    return res.send(locateUser(customers, req.params.id));
-  }
-  return res.send('Usuário não encontrado');
+app.get('/account/client/:id', checkExistClient, (req, res) => {
+  const client = customers.find((customer) => customer.id === req.params.id);
+  return res.send(client);
 });
 
 app.get('/account/client/extract/:id', (req, res) => {
-  const client = locateUser(customers, req.params.id);
+  const client = checkExistClient(customers, req.params.id);
   const extract = customers.find((customer) => {
     return customer.cpf === client.cpf;
   });
@@ -94,7 +100,7 @@ app.get('/account/client/extract/:id', (req, res) => {
 });
 
 app.get('/account/client/extract/:id/:date', (req, res) => {
-  const client = locateUser(customers, req.params.id);
+  const client = checkExistClient(customers, req.params.id);
   for (let i = 0; i < customers.length; i++) {
     if (customers[i].id == client.id) {
       for (let j = 0; j < customers.extrato.length; j++) {
@@ -128,11 +134,11 @@ app.put('/account/client/change/:id', (req, res) => {
 });
 
 app.delete('/account/client/delete/:id', (req, res) => {
-  if (locateUser(customers, req.params.id)) {
-    customers.splice(locateUser(customers, req.params.id), 1);
+  if (checkExistClient(customers, req.params.id)) {
+    customers.splice(checkExistClient(customers, req.params.id), 1);
     return res.send('Usuário apagado com sucesos.');
   }
   return res.send('Usuário não encontrado');
 });
 
-app.listen(3030);
+module.exports = app;
